92e26cfe2c5f12bbd9abe2a8f0c323a0a591f416
SuperKooks <superkooks@superkooks.com>
2019-01-06 15:50:05 +1100

    init

---
+++ java/com/superkooks/advancedteleportation/AdvancedTeleportation.java
@@ -0,0 +1,15 @@
+package com.superkooks.advancedteleportation;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = AdvancedTeleportation.MODID, version = AdvancedTeleportation.VERSION)
+public class AdvancedTeleportation {
+	public static final String MODID = "advancedteleportation";
+	public static final String VERSION = "0.1";
+	
+	@Mod.EventHandler
+	public void init(FMLInitializationEvent event) {
+		// lol
+	}
+}
